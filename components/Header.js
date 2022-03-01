9a28bee442ff61c910077e2d04f6d83f4e82418a
irfan dayan <engr.irfandayan@gmail.com>
2022-03-01 15:06:08 +0500

    Add heroicons

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,34 @@
+import Image from "next/image";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+import HeaderItem from "./HeaderItem";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTION" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="/images/logo.svg"
+        width={200}
+        height={100}
+      />
+    </header>
+  );
 };
 
 export default Header;
